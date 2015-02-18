6bd185508e31d7f9a16f9a8c113c1aa9a5c500c0
blade <blade@sanemail.de>
2015-02-18 13:35:30 +0100

    bar

--- sparktest/cidresque_ip.ads
+++ sparktest/cidresque_ip.ads
@@ -12,12 +12,21 @@ is
 
    type Inet4num   is mod 2 ** 32;
 
+   function BitWiseOr (A,B : Unsigned_32) return Unsigned_32
+	with Contract_Cases =>
+	(
+		(A /= 0) or   (B /= 0) => BitWiseOr'Result /= 0,
+		(A  = 0) and  (B  = 0) => BitWiseOr'Result  = 0
+
+	),
+	Global => null,
+	Depends => (BitWiseOr'Result => (A,B));
+
    function toInet4num (OA : OctetArray) return Inet4num
 	with Contract_Cases =>
        (
-          True => True
-			--OA(4) = 0 and OA(3) = 0 and OA(2) = 0 and OA(1) = 0      => toInet4num'Result = 0,
-			--(OA(1) + OA(2) + OA(3) + OA(4)) >= 1 => toInet4num'Result >= 0
+          (OA(4) = 0)  and (OA(3) = 0) and (OA(2) = 0) and ( OA(1) = 0)      => toInet4num'Result = 0,
+          (OA(4) /= 0) or (OA(3) /= 0) or (OA(2) /= 0) or (OA(1) /= 0)      => toInet4num'Result /= 0
    	),
 	Global => null,
 	Depends => (toInet4num'Result => OA);
