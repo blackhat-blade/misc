ea0971e9fb8a1d529977cb3eb80633625e1b7e09
blade <blade@sanemail.de>
2015-02-26 04:08:51 +0100

    testing test 2

--- smca-pub/snmp/test2.pl
+++ smca-pub/snmp/test2.pl
@@ -5,7 +5,17 @@ use warnings;
 
 use Data::Dumper;
 
-my $file = shift;
-my $data = do $file or die "oops: $!/$@";
+my $file   = shift;
+my $module = shift;
 
-print Dumper ($data);
+my $data = do $file or die "oops: $!/$@";
+my $mod;
+
+
+die "no module given " unless $module;
+
+die "module $module not found" unless $data->{$module};
+
+$mod = $data->{$module};
+
+print Dumper ($mod);
