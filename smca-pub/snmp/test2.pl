1d830ebe008e5e3c70830ee4746f0e82d7f324a5
blade <blade@sanemail.de>
2015-02-26 05:30:35 +0100

    testing test 2

--- smca-pub/snmp/test2.pl
+++ smca-pub/snmp/test2.pl
@@ -6,6 +6,8 @@ use warnings;
 use Data::Dumper;
 
 use Moose::Meta::Class;
+#use Moose::Meta::Attribute;
+
 
 my $file       = shift;
 my $moduleName = shift;
@@ -24,12 +26,19 @@ die "module $moduleName not found" unless $data->{$moduleName};
 
 $moduleData = $data->{$moduleName};
 
-#print Dumper ($moduleData );
+print Dumper ($moduleData );
 
 $moduleClassName = join '::', map {ucfirst lc } split '-', $moduleName;
 
 say "moduleClassName = $moduleClassName"; 
 
 $moduleClass = Moose::Meta::Class->create($moduleClassName);
+
+
+
+$moduleClass->add_attribute('module', is => 'ro', default => $moduleName ); 
+#Moose::Meta::Attribute->new( 'module', is => 'ro');
+#$moduleClass->add_attribute();
+
 
 print Dumper ($moduleClass );
