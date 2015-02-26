b37a5f9b691971982fd8ee79f75346beb76343dd
blade <blade@sanemail.de>
2015-02-26 03:55:28 +0100

    testing test 2

--- smca-pub/snmp/test2.pl
+++ smca-pub/snmp/test2.pl
@@ -6,6 +6,6 @@ use warnings;
 use Data::Dumper;
 
 my $file = 'mib/pg.stat.activity.dmp.pl';
-my $data = do $file or die "oops: $@";
+my $data = do $file or die "oops: $!/$@";
 
 print Dumper ($data);
