c21ccee29080baec2c829ee7131c642b5f8d3b6d
blade <blade+gl@sanemail.de>
2015-05-29 16:48:17 +0200

    dirtest 5

--- confuse/confuse.pl
+++ confuse/confuse.pl
@@ -60,6 +60,8 @@ sub e_getdir
 {
 	my $path = filename_fixup(shift);
 
+	print "getdir called for ", Dumper($path), "\n";
+
 	if (@{$path} == 1 && $path->[0] eq '')
 	{
 		return $root->getall(), 0;
